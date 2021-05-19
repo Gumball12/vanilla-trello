62bdebefd2d411b8fc3e8b5c74b08dc7bd49a6d2
shj <kan02134@gmail.com>
2021-05-19 23:13:16 +0900

    feat: implement textarea-box component

---
+++ workspaces/frontend/src/components/share/TextareaBox.js
@@ -0,0 +1,45 @@
+import mvvm from '/src/share/MvvmHtmlElement/index.js';
+
+const html = `
+<textarea m-ref="textarea" class="text" @input="updateValue"></textarea>
+
+<style scoped>
+@import url('/src/styles/components.css');
+@import url('/src/styles/typo.css');
+
+textarea {
+  width: 100%;
+  min-width: 100%;
+  max-width: 100%;
+  height: 100%;
+  padding: 0;
+  border: none;
+  outline: none;
+  background-color: transparent;
+  font-family: 'Nanum Gothic', sans-serif;
+  color: var(--black);
+}
+</style>
+`;
+
+window.customElements.define(
+  'textarea-box',
+  class extends mvvm {
+    constructor() {
+      super({
+        html,
+        data: {
+          insert: '',
+        },
+        methods: {
+          updateValue: ({ target: { value } }) => {
+            this.$data.insert = value;
+          },
+          focus: () => {
+            this.$ref.textarea.focus();
+          },
+        },
+      });
+    }
+  },
+);
