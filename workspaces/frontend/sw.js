9e098c6d6b75bf6f9913750779f693d6d8ec72a4
shj <kan02134@gmail.com>
2021-05-22 22:16:46 +0900

    feat: make assets cached

--- workspaces/frontend/sw.js
+++ workspaces/frontend/sw.js
@@ -1,6 +1,10 @@
+// set cache name
+const CACHE_NAME = 'mini-trello-v1';
+
+// service worker installation
 self.addEventListener('install', evt =>
   evt.waitUntil(
-    caches.open('mini-trello-v1').then(cache =>
+    caches.open(CACHE_NAME).then(cache =>
       // caching assets
       cache.addAll([
         '/',
@@ -8,5 +12,27 @@ self.addEventListener('install', evt =>
         'https://fonts.googleapis.com/css2?family=Material+Icons',
       ]),
     ),
+  ),
+);
+
+// hooking fetch evt
+self.addEventListener('fetch', evt =>
+  evt.respondWith(
+    caches.match(evt.request).then(resp => {
+      // if already cached
+      if (resp !== undefined) {
+        return resp;
+      }
+
+      // or not => try caching
+      return fetch(evt.request).then(resp => {
+        const respClone = resp.clone();
+        caches
+          .open(CACHE_NAME)
+          .then(cache => cache.put(evt.request, respClone));
+
+        return resp;
+      });
+    }),
   ),
 );
